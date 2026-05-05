f73cb3f032c682000b6610291890170fa5ce56b3
dcareerhub-ui <dcareerhub@gmail.com>
2026-05-05 10:40:09 +0500

    Updated IMPLEMENTATION_IMPROVEMNETS.md

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import Problem from './components/Problem';
 import Services from './components/Services';
 import BeforeAfter from './components/BeforeAfter';
 import WhyUs from './components/WhyUs';
-import HowItWorks from './components/HowItWorks';
 import Reviews from './components/Reviews';
 import About from './components/About';
 import ServiceMap from './components/ServiceMap';
-import FAQ from './components/FAQ';
 import CTA from './components/CTA';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -23,11 +21,9 @@ function App() {
       <Services />
       <BeforeAfter />
       <WhyUs />
-      <HowItWorks />
       <Reviews />
       <About />
       <ServiceMap />
-      <FAQ />
       <CTA />
       <Contact />
       <Footer />
